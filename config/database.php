9ebc29247eb963f8cb91f0f37fa5939b2443a935
maejimayuto <maejimayuto@gmail.com>
2019-03-16 19:52:15 +0900

    add comment & comment out unuse ENV

--- config/database.php
+++ config/database.php
@@ -57,6 +57,7 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
+            // ここは前島の環境に合わせて追加した
             'unix_socket' => '/tmp/mysql.sock',
         ],
 
