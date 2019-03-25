8d58747864213c95488efb236bcf079b5387db76
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-03-26 02:41:23 +0900

    fix migration class name

--- database/migrations/2019_03_23_131659_create_facility_staffs_table.php
+++ database/migrations/2019_03_23_131659_create_facility_staffs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTFacilityStaffTable extends Migration
+class CreateFacilityStaffsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2019_03_23_131729_create_staff_auths_table.php
+++ database/migrations/2019_03_23_131729_create_staff_auths_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateTStaffAuthTable extends Migration
+class CreateStaffAuthsTable extends Migration
 {
     /**
      * Run the migrations.
