c43335ee8929b5a3b2800d98a739e9587164041c
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-03-29 21:41:11 +0900

    add seeder

---
+++ database/seeds/StaffsTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class StaffsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('staffs')->insert([
+            'name' => 'epark-dev',
+            'email' => 'epark-dev@example.com',
+            'password' => bcrypt('PassW@rd01'),
+            'authority' => '1'
+        ]);
+    }
+}
