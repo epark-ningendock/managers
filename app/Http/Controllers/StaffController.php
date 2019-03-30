788c7013e45b16106d0793af21e2d029d660727a
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-03-30 13:59:45 +0900

    add basic factory, seeder, controller

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class StaffController extends Controller
-{
+{   
     public function index () 
     {
         $hello = 'Hello,World!';
---
+++ database/factories/FacilityStaffFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\FacilityStaff::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
