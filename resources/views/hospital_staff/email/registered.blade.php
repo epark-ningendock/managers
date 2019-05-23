84500e02bbe1817b934c57d9d6167a4918578cfe
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-05-23 20:37:25 +0900

    [add] email sentence, reset_token migrartion.

--- resources/views/hospital_staff/email/registered.blade.php
+++ resources/views/hospital_staff/email/registered.blade.php
@@ -1,5 +1,15 @@
 <html>
     <body>
-      <p>Thank you</p>
+      <p>{{ $name = "西村竜" }}さん ご登録ありがとうございます！</p>
+      <br>
+      <p>あなたのパスワードは</p>
+      <br>
+      <p>{{ $password = "PazzW@rd01" }} です</p>
+      <br>
+      <p>次のリンクからログインすることができます。</p>
+      <br>
+      <a href="#">#</a>
+      <br>
+      <p>これからもEparkをご利用ください！</p>
     </body>
 </html>
