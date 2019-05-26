056d59afba0d6e50dedbe4fe22157badc1002b46
matsuda chihiro <seven.mazda@gmail.com>
2019-05-26 16:01:28 +0900

    #72 customers table migraiton

--- database/migrations/2019_05_16_030326_customers_table.php
+++ database/migrations/2019_05_16_030326_customers_table.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Helpers\DBCommonColumns;
 
 class CustomersTable extends Migration
 {
+    use DBCommonColumns;
     /**
      * Run the migrations.
      *
@@ -13,7 +15,27 @@ class CustomersTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('parent_customer_id')->unsigned()->nullable();
+            $table->integer('member_number')->unsigned()->nullable();
+            $table->string('registration_card_number',32)->nullable();
+            $table->string('name',65)->nullable();
+            $table->string('name_kana',65)->nullable();
+            $table->string('tel',13)->nullable();
+            $table->string('email',255)->nullable();
+            $table->string('postcode',8)->nullable();
+            $table->integer('prefecture_id')->nullable();
+            $table->string('address',200)->nullable();
+            $table->char('sex',1)->nullable();
+            $table->string('birthday',8)->unsigned()->nullable();
+            $table->string('memo',255)->nullable();
+            $table->integer('claim_count')->unsigned()->default(0);
+            $table->integer('recall_count')->unsigned()->default(0);
+            $table->unique('email');
+            $table->unique('member_number');
+            $this->addCommonColumns($table);
+        });
     }
 
     /**
@@ -23,6 +45,6 @@ class CustomersTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('customers');
     }
 }
