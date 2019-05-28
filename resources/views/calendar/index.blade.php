a5e0a04ea91f23d6507606311aff55dcefa376af
than htet aung <thanhtetaung87@gmail.com>
2019-05-28 17:50:11 +0630

    changed listing design and remove sort button

--- resources/views/calendar/index.blade.php
+++ resources/views/calendar/index.blade.php
@@ -18,58 +18,46 @@
 @section('search')
   <h4>カレンダーの登録</h4>
   <div class="mb-4">下記のボタンをクリックすると新しいカレンダーを登録することができます。</div>
-  <a class="btn btn-primary" href="{{ route('calendar.create') }}">カレンダーの新規登録</a>
+  <a class="btn btn-success" href="{{ route('calendar.create') }}">カレンダーの新規登録</a>
   <a class="btn btn-primary ml-2">休日確定</a>
 @stop
 
 @section('table')
-  <h4>カレンダーの一覧と編集</h4>
-  <div class="mb-4">カレンダーの並び替えボタンを押下するとカレンダーの順番を並び替えることができます。</div>
-  <a class="btn btn-success">カレンダーの並び替え</a>
   <div class="box box-solid mt-4" id="accordion">
     <table class="table table-bordered">
       <tbody>
       @foreach($calendars as $index => $calendar)
         <tr>
-          <td>
+          <td colspan="3" style="background-color:#f4f4f4;">
             <div>
               <h4 style="display:inline-block;">
-                <a data-toggle="collapse" href="#collapse_{{ $index }}"
-                   aria-expanded="false">
+                <a href="#">
                   {{ $calendar->name }}
                 </a>
                 「カレンダー受付可否 : {{ $calendar->is_calendar_display->description }}」
               </h4>
-              <button class="btn btn-default pull-right">カレンダー確定</button>
-            </div>
-
-            <div id="collapse_{{ $index }}" class="panel-collapse collapse">
-              <hr style="margin: 5px -10px;"/>
-              @if($calendar->courses->isNotEmpty())
-                <div class="table-responsive mt-4">
-                  <table id="example2" class="table table-bordered">
-                    <thead>
-                    <tr>
-                      <th>検査コースID</th>
-                      <th>検査コース名</th>
-                      <th>WEB受付</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($calendar->courses as $course)
-                      <tr>
-                        <td>{{ $course->id }}</td>
-                        <td>{{ $course->name }}</td>
-                        <td>{{ $course->web_reception->description }}</td>
-                      </tr>
-                    @endforeach
-                    </tbody>
-                  </table>
-                </div>
-              @endif
+              <button class="btn btn-primary pull-right">カレンダー確定</button>
             </div>
           </td>
         </tr>
+        @if($calendar->courses->isNotEmpty())
+          <tr>
+            <th>検査コースID</th>
+            <th>検査コース名</th>
+            <th>WEB受付</th>
+          </tr>
+          @foreach($calendar->courses as $course)
+            <tr>
+              <td>{{ $course->id }}</td>
+              <td>{{ $course->name }}</td>
+              <td>{{ $course->web_reception->description }}</td>
+            </tr>
+          @endforeach
+        @else
+          <tr>
+            <td colspan="3"></td>
+          </tr>
+        @endif
       @endforeach
       </tbody>
     </table>
