8fbb15ad8552e10f3f0f1e484a947c5d069455b5
f-shun <43958704+f-shun@users.noreply.github.com>
2019-06-01 17:24:13 +0900

    add hospirail_id to hospital_staffs

--- database/seeds/HospitalStaffsTableSeeder.php
+++ database/seeds/HospitalStaffsTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Hospital;
+use App\HospitalStaff;
 
 class HospitalStaffsTableSeeder extends Seeder
 {
@@ -11,7 +13,13 @@ class HospitalStaffsTableSeeder extends Seeder
      */
     public function run()
     {
-	    factory(App\HospitalStaff::class, 50)->create();
+      $hospitals = Hospital::all();
+      HospitalStaff::unguard();
+      DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
+      foreach ($hospitals as $hospital) {
+	      factory(HospitalStaff::class, 50)->create();
+      }
 
       // TODO　動作確認用 マージ前に削除
       DB::table('hospital_staffs')->insert([
@@ -19,7 +27,10 @@ class HospitalStaffsTableSeeder extends Seeder
         'name' => 'user',
         'email' => 'user@example.com',
         'password' => Hash::make('1111'),
+        'hospital_id' => '1',
       ]);
 
+      DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+      HospitalStaff::reguard();
     }
 }
