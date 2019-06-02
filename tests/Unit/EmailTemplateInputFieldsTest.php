395111192fc8a80d0b36f6ef2a29c8548c200ac9
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-06-02 23:42:38 +0900

    [add] EmailTemplateTest. [delete] unuse Test file/

--- tests/Unit/EmailTemplateInputFieldsTest.php
+++ tests/Unit/EmailTemplateInputFieldsTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class StaffTest extends TestCase
+class EmailTemplateInputFieldsTest extends TestCase
 {
     /**
      * A basic test example.
