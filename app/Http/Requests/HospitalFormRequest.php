7a0696afdaa9d5ca5eea0db0aafa57e686974333
matsuda chihiro <seven.mazda@gmail.com>
2019-06-21 11:28:21 +0900

    HospitalFormRequest

---
+++ app/Http/Requests/HospitalFormRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+use App\Enums\HospitalEnums;
+
+class HospitalFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $status = HospitalEnums::getValues();
+        return [
+            'status' => Rule::in($status),
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'status.in' => '「状態」を正しく選択してください。',
+        ];
+    }
+}
