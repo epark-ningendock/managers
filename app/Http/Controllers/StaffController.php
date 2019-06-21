e920974fc0ba25c89a53644172a76e90cee9fe82
matsuda chihiro <seven.mazda@gmail.com>
2019-06-21 12:19:18 +0900

    #162 staff search request

--- app/Http/Controllers/StaffController.php
+++ app/Http/Controllers/StaffController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enums\Authority;
 use App\Enums\StaffStatus;
 use App\Http\Requests\StaffFormRequest;
+use App\Http\Requests\StaffSearchFormRequest;
 use App\Staff;
 use App\StaffAuth;
 use Illuminate\Http\Request;
@@ -22,10 +23,10 @@ class StaffController extends Controller
 
     /**
      * スタッフ一覧の表示
-     * @param Request $request
+     * @param StaffSearchFormRequest $request
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function index(Request $request)
+    public function index(StaffSearchFormRequest $request)
     {
         $query = Staff::query();
         if ($request->input('name', '') != '') {
