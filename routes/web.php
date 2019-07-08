825964ec428cd5a9a840fbaee2da9bb1df784ce7
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-07-08 22:00:07 +0900

    [delete] comment

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,6 @@ Route::get('/hospital/contract-information', 'ContractInformationController@crea
 Auth::routes();
 Route::get('/login', 'Auth\LoginController@getLogin')->name('login');
 Route::post('/login', 'Auth\LoginController@postLogin');
-// Route::post('/logout', 'Auth\LoginController@logout');
 
 /*
 |--------------------------------------------------------------------------
