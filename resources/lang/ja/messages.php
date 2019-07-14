a6748ac69fee104836f96028f642faa3279ce662
f-shun <43958704+f-shun@users.noreply.github.com>
2019-07-14 20:31:04 +0900

    fix hospital_staff_form

--- resources/lang/ja/messages.php
+++ resources/lang/ja/messages.php
@@ -18,7 +18,7 @@ return [
     |
     */
     
-    'created' => ':nameが登録しました。',
+    'created' => ':nameを登録しました。',
     'updated' => ':nameを更新しました。',
     'remove' => '削除する',
     'deleted' => '削除が完了しました。',
