487e1177e16ec363e0849ef5840b02e9886a52d0
than htet aung <thanhtetaung87@gmail.com>
2019-07-16 02:07:13 +0630

    - fixed soft delete restore - fixed initial select for major and middle classification by adding empty option in classification sort

--- resources/views/classification/sort.blade.php
+++ resources/views/classification/sort.blade.php
@@ -47,7 +47,9 @@
       <div class="col-md-2">
         <div class="form-group">
           <label for="major">大分類</label>
-          <select class="form-control" id="major" name="major" disabled>
+          <select class="form-control" id="major" name="major" disabled value="">
+            <!-- empty option to protect initial first option select -->
+            <option class="empty"></option>
             @foreach($c_majors as $c_major)
               <option data-type-id="{{ $c_major->classification_type_id }}"
                   value="{{ $c_major->id }}" {{ (isset($major) && $major == $c_major->id) ? "selected" : "" }}>{{ $c_major->name }}</option>
@@ -58,7 +60,9 @@
       <div class="col-md-2">
         <div class="form-group">
           <label for="middle">中分類</label>
-          <select class="form-control" id="middle" name="middle" disabled>
+          <select class="form-control" id="middle" name="middle" disabled value="">
+            <!-- empty option to protect initial first option select -->
+            <option class="empty"></option>
             @foreach($c_middles as $c_middle)
               <option data-major-id="{{$c_middle->major_classification_id}}"
                   value="{{ $c_middle->id }}" {{ (isset($middle) && $middle == $c_middle->id) ? "selected" : "" }}>{{ $c_middle->name }}</option>
@@ -130,6 +134,8 @@
           -----------------------------------------------------*/
           (function () {
               const cEle = $('#classification');
+              // remove empty options.
+              $('#major option.empty, #middle option.empty').remove();
               const onChange = function () {
                   if (cEle.val() == 'major') {
                       $('#major, #middle').attr('disabled', 'disabled');
