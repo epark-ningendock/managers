89dad67ed0cccc828168cbf89be179772015a27e
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-07-17 01:11:56 +0900

    review No24 + fix email subuject

--- resources/views/staff/partials/form.blade.php
+++ resources/views/staff/partials/form.blade.php
@@ -21,31 +21,35 @@
         {{ StaffStatus::Invalid()->description }}
       </label>
     </div>
+    @if ($errors->has('status')) <p class="help-block has-error">{{ $errors->first('status') }}</p> @endif
   </div>
 
-  <div class="form-group">
-    <label for="name">スタッフ名</label>
+  <label for="name">スタッフ名</label>  
+  <div class="form-group @if ($errors->has('name')) has-error @endif">
     <input type="text" class="form-control text" id="name" name="name"
            value="{{ old('name', (isset($staff) ? $staff->name : null)) }}"
            placeholder="スタッフ名">
+    @if ($errors->has('name')) <p class="help-block">{{ $errors->first('name') }}</p> @endif
   </div>
 
-  <div class="form-group">
-    <label for="login_id">ログインID</label>
+  <label for="login_id">ログインID</label>
+  <div class="form-group  @if ($errors->has('login_id')) has-error @endif">
     <input type="text" class="form-control text" id="login_id" name="login_id"
            value="{{ old('login_id', (isset($staff) ? $staff->login_id : null)) }}"
            placeholder="ログインID"> 
+    @if ($errors->has('login_id')) <p class="help-block">{{ $errors->first('login_id') }}</p> @endif
   </div>
 
-  <div class="form-group">
-    <label for="email">メールアドレス</label>
+  <label for="email">メールアドレス</label>
+  <div class="form-group  @if ($errors->has('email')) has-error @endif">
     <input type="email" class="form-control text" id="email" name="email"
            value="{{ old('email', (isset($staff) ? $staff->email : null)) }}"
            placeholder="メールアドレス">
+    @if ($errors->has('email')) <p class="help-block">{{ $errors->first('email') }}</p> @endif
   </div>
 
   <label for="department">部署</label>
-  <select name="department_id" id="department_id" class="form-control select-box">
+  <select name="department_id" id="department_id" class="form-control select-box  @if ($errors->has('department_id')) has-error @endif">
       <option value=""></option>
       @foreach($departments as $department)
           <option value="{{ $department->id }}"
@@ -55,9 +59,10 @@
           >{{ $department->name }}</option>
       @endforeach
   </select>
+  @if ($errors->has('department_id')) <p class="help-block">{{ $errors->first('department_id') }}</p> @endif
 
+  <label class="mt-3">スタッフ権限</label>
   <div class="form-group mt-3">
-    <label class="mb-0">スタッフ権限</label>
     <div class="radio mt-0">
       @if (Auth::user()->authority->value === Authority::Admin)
         <label>
@@ -84,7 +89,9 @@
         {{ Authority::ContractStaff()->description }}
       </label>
     </div>
+    @if ($errors->has('authority')) <p class="help-block has-error">{{ $errors->first('authority') }}</p> @endif
   </div>
+
 
   <div class="form-group">
     <label class="mb-0">医療機関管理</label>
@@ -107,6 +114,7 @@
         {{ Permission::Edit()->description }}
       </label>
     </div>
+    @if ($errors->has('is_hospital')) <p class="help-block has-error">{{ $errors->first('is_hospital') }}</p> @endif
   </div>
 
   <div class="form-group">
@@ -128,6 +136,7 @@
         {{ Permission::Edit()->description }}
       </label>
     </div>
+    @if ($errors->has('is_staff')) <p class="help-block has-error">{{ $errors->first('is_staff') }}</p> @endif
   </div>
 
   <div class="form-group">
@@ -151,6 +160,7 @@
         {{ Permission::Edit()->description }}
       </label>
     </div>
+    @if ($errors->has('is_cource_classification')) <p class="help-block has-error">{{ $errors->first('is_cource_classification') }}</p> @endif
   </div>
 
   <div class="form-group">
@@ -177,6 +187,7 @@
         {{ Permission::Upload()->description }}
       </label>
     </div>
+    @if ($errors->has('is_invoice')) <p class="help-block has-error">{{ $errors->first('is_invoice') }}</p> @endif
   </div>
 
   <div class="form-group">
@@ -203,6 +214,7 @@
         {{ Permission::Upload()->description }}
       </label>
     </div>
+    @if ($errors->has('is_pre_account')) <p class="help-block has-error">{{ $errors->first('is_pre_account') }}</p> @endif
   </div>
 
   <div class="form-group">
@@ -229,6 +241,7 @@
         {{ Permission::Upload()->description }}
       </label>
     </div>
+    @if ($errors->has('is_contract')) <p class="help-block has-error">{{ $errors->first('is_contract') }}</p> @endif
   </div>
 
 
