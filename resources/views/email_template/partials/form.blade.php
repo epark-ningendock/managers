d6faa6e58ed8625f92752929dd4f6c74bc0c9c55
maejimayuto <maejimayuto@gmail.com>
2019-08-18 00:33:41 +0900

    fix validation

--- resources/views/email_template/partials/form.blade.php
+++ resources/views/email_template/partials/form.blade.php
@@ -5,13 +5,13 @@
       <p class="text-bold">差出人メールアドレス：unei@eparkdock.com</p>
       <div class="form-group py-sm-1 @if ($errors->has('title')) has-error @endif">
         <label for="title">テンプレート名（件名）<span class="form_required">必須</span></label>
-        {{ Form::text('title', (isset($email_template->title) ) ? $email_template->title : Input::old('title'), ['class' => 'form-control', 'id' => 'title', 'placeholder' => '件名を入力してください']) }}
+        {{ Form::text('title', old('title', (isset($email_template->title) ) ? $email_template->title : null), ['class' => 'form-control', 'id' => 'title', 'placeholder' => '件名を入力してください']) }}
         @if ($errors->has('title')) <p class="help-block"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>{{ $errors->first('title') }}</p> @endif
       </div>
         <div class="form-group py-sm-1 @if ($errors->has('text')) has-error @endif">
             <label for="text">本文</label>
-            {{ Form::textarea('text', (isset($email_template) ) ? $email_template->text : null, ['class' => 'form-control', 'id' => 'text', 'rows' => '10', 'placeholder' => '本文を入力してください']) }}
-            <span class="pull-right">0/20000文字</span>
+            {{ Form::textarea('text', old('text',(isset($email_template) ) ? $email_template->text : null), ['class' => 'form-control', 'id' => 'text', 'rows' => '10', 'placeholder' => '本文を入力してください']) }}
+            <span class="pull-right">{{ strlen(old('text', (isset($email_template) ) ? $email_template->text : null)) }}/20000文字</span>
             @if ($errors->has('text')) <p class="help-block"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>{{ $errors->first('text') }}</p> @endif
         </div>
 
@@ -32,7 +32,7 @@
               $('textarea').on('keyup', function() {
                   const len = $(this).val().length;
                   if (len > 20000) {
-                      $(this).val($(this).val().substring(0, 19999));
+                      $(this).val($(this).val().substring(0, 20000));
                   } else {
                       $(this).next('span').text(len + '/20000文字');
                   }
