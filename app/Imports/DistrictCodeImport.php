6b73a4564f0084aae276dab424ef387edc822f0f
ShibuyaKosuke <shibuya@imagenious.co.jp>
2019-08-19 19:11:54 +0900

    add DistrictCodeImport

---
+++ app/Imports/DistrictCodeImport.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Imports;
+
+use App\DistrictCode;
+use Maatwebsite\Excel\Row;
+
+class DistrictCodeImport extends ImportAbstract
+{
+    /**
+     * 旧システムのインポート対象テーブルのプライマリーキーを返す
+     * Returns the primary key of the import target table of the old system
+     * @return string
+     */
+    public function getOldPrimaryKeyName(): string
+    {
+        return '';
+    }
+
+    /**
+     * 新システムの対象クラス名を返す
+     * Returns the target class name of the new system
+     * @return string
+     */
+    public function getNewClassName(): string
+    {
+        return DistrictCode::class;
+    }
+
+    /**
+     * @param Row $row
+     * @return mixed
+     */
+    public function onRow(Row $row)
+    {
+        $row = $row->toArray();
+
+        $model = new DistrictCode([
+        ]);
+    }
+}
