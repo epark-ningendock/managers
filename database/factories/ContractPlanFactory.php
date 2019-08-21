c57f58c5d67f3324e0f092643cad25de40d6aa66
than htet aung <thanhtetaung87@gmail.com>
2019-08-21 15:02:58 +0630

    - fixed seeder value for fee rate

--- database/factories/ContractPlanFactory.php
+++ database/factories/ContractPlanFactory.php
@@ -7,7 +7,7 @@ $factory->define(ContractPlan::class, function (Faker $faker) {
     return [
         'plan_code' => $faker->unique()->randomElement(['01', '02', '03', '04', '05', '06', '07', '08', '09', '10']),
         'plan_name' => $faker->unique()->userName,
-        'fee_rate' => $faker->randomNumber(),
+        'fee_rate' => $faker->numberBetween(0, 100),
         'monthly_contract_fee' => $faker->randomNumber()
     ];
 });
