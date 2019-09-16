da245c753e18328b55741aa715a347b785800006
Ryu Nishimura <ryryrnsmr201@gmail.com>
2019-09-16 22:09:58 +0900

    [fix] メール本文

--- resources/views/hospital_email_setting/email/operation-mail.blade.php
+++ resources/views/hospital_email_setting/email/operation-mail.blade.php
@@ -1,16 +1,10 @@
 <html>
     <body>
-      @if ($data['processing'] === "登録")
-        <p>登録者：{{ $data['staff_name'] }}</p>
-        <p>登録日時：{{ $data['hospital_email_setting']->created_at }}</p>
-      @elseif ($data['processing'] === "更新")
-        <p>更新者：{{ $data['staff_name'] }}</p>
-        <p>更新日時：{{ $data['hospital_email_setting']->updated_at }}</p>
-      @endif
+      <p>受付メール設定の情報が登録・更新されました。</p>
+      <p>登録・更新者：{{ $data['staff_name'] }}</p>
+      <p>登録・更新日時{{ $data['hospital_email_setting']->update_at }}</p>
       <br>
+      <p>詳細の確認は管理画面より行なってください。</p>
       <a href="{{url('login')}}">{{url('login')}}</a>
-      <br>
-      <p>本メールにお心当たりがない場合は、お手数ですが削除してください。</p>
-      <p>本メールは自動配信です。ご返信いただいてもご質問等にはご回答できませんのでご了承ください。</p>
     </body>
 </html> 
