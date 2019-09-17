d4d7d4f553a490b1ad21736e307838ce333e1976
hlaporthein <hlaporthein@gmail.com>
2019-09-17 22:03:50 +0630

    db seeder fixed on billing

--- database/factories/BillingFactory.php
+++ database/factories/BillingFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Billing::class, function (Faker $faker) {
     $randomDate = random_int(1, 100);
     return [
-        'hospital_id' => factory(Hospital::class)->create()->id,
+        'hospital_id' => $faker->numberBetween(1, 50),
         'contract_plan_id' => factory(ContractPlan::class)->create()->id,
         'from' => now()->addDay($randomDate),
         'to' => now()->addDay($randomDate + 21),
--- database/factories/ContractPlanFactory.php
+++ database/factories/ContractPlanFactory.php
@@ -6,7 +6,7 @@ use App\ContractPlan;
 $factory->define(ContractPlan::class, function (Faker $faker) {
     return [
         'plan_code' => $faker->unique()->randomElement(['Y001', 'Y002', 'Y003', 'Y004', 'Y005', 'Y006', 'Y007', 'Y008', 'Y009', 'Y010']),
-        'plan_name' => $faker->unique()->userName,
+        'plan_name' => $faker->unique(true)->userName,
         'fee_rate' => $faker->numberBetween(0, 100),
         'monthly_contract_fee' => $faker->randomNumber()
     ];
