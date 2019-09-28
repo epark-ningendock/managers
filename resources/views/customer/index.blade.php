c9ab10b6d3c80b73bc6dfb8bd8a7015bd12ca6f2
maejimayuto <maejimayuto@gmail.com>
2019-09-28 20:21:55 +0900

    bugfix

--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -43,13 +43,7 @@
                 </th>
                 <th>{{ trans('messages.phone_number') }}</th>
                 <th>{{ trans('messages.birthday') }}</th>
-                {{--<th>--}}
-                    {{--<a href="{{ route('customer.index', ['email_sorting' => columnSorting('email_sorting')]) }}">--}}
-                        {{--{{ trans('messages.email') }}--}}
-                    {{--</a>--}}
-                {{--</th>--}}
                 <th>{{ trans('messages.gender') }}</th>
-                <th>生年月日</th>
                 <th></th>
                 <th></th>
             </tr>
@@ -62,9 +56,6 @@
                     <tr class="customer-{{ $customer->id }}">
                         <td>{{ $customer->id }}</td>
                         <td>
-                            {{ $customer->id }}
-                        </td>
-                        <td>
                             <a class="detail-link" href="#" data-id="{{ $customer->id }}" data-route="{{ route('customer.detail') }}">
                                 {{ $customer->name }}
                             </a>
@@ -73,7 +64,6 @@
                         <td>{{ $customer->tel }}</td>
                         <td>{{ $customer->birthday }}</td>
                         <td>{{ $customer->sex->description or '-' }}</td>
-                        <td>{{ $customer->birthday }}</td>
                         <td>
                             <a class="btn btn-primary"
                                href="{{ route('customer.edit', $customer->id) }}">
