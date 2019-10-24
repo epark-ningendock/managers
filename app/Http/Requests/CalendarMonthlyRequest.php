b8807dad94d0439216e8e2316f865298340350eb
tarokimura87 <taro.palooza.08@gmail.com>
2019-10-25 08:44:59 +0900

    make it better

--- app/Http/Requests/CalendarMonthlyRequest.php
+++ app/Http/Requests/CalendarMonthlyRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use Carbon\Carbon;
 use Log;
 class CalendarMonthlyRequest extends ValidationRequest
 {
@@ -18,9 +19,6 @@ class CalendarMonthlyRequest extends ValidationRequest
             // 検査コース空満情報（月別）
             'get_yyyymm_from' => 'nullable|date_format:Ym',
             'get_yyyymm_to' => 'nullable|date_format:Ym',
-            // 検査コース空満情報（日別）
-            'get_yyyymmdd_from' => 'nullable|date_format:Ymd',
-            'get_yyyymmdd_to' => 'nullable|date_format:Ymd',
 
         ];
     }
@@ -32,16 +30,26 @@ class CalendarMonthlyRequest extends ValidationRequest
      */
     protected function getBetween()
     {
-        $now = date('Ymd');
-        $from = $this->input('get_yyyymm_from') != null ? $this->input('get_yyyymm_from').'01' : $now;
-        $to = $this->input('get_yyyymm_to') != null ? $this->input('get_yyyymm_to').'01' : $now;
-        date('Ymd', strtotime('+5 month' . $to));
+        $from = Carbon::today()->firstOfMonth();
+        $to = Carbon::today();
+        $to->addMonthNoOverflow(5);
+        $to = $to->endOfMonth();
+        if (!empty($this->input('get_yyyymm_from'))) {
+            $from = Carbon::createMidnightDate(
+                substr($this->input('get_yyyymm_from'), 0, 4),
+                substr($this->input('get_yyyymm_from'), -2, 2),
+                1);
+        }
 
-        // 月初/月末セット
-        $firstDate = date('Ymd', strtotime('first day of ' . $from));
-        $lastDate = date('Ymd', strtotime('last day of ' . $to));
+        if (!empty($this->input('get_yyyymm_to'))) {
+            $to = Carbon::createMidnightDate(
+                substr($this->input('get_yyyymm_to'), 0, 4),
+                substr($this->input('get_yyyymm_to'), -2, 2),
+                25);
+            $to->addMonthNoOverflow();
+        }
 
-        return (object) ['from' => $firstDate, 'to' => $lastDate];
+        return (object) ['from' => $from, 'to' => $to];
     }
     /**
      * 検査コース空満情報取得API request to array
