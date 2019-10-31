ea4d8b38f14938e0c4dd1cdfc28cb1eeda130c34
matsuda chihiro <seven.mazda@gmail.com>
2019-11-01 05:50:17 +0900

    redirect https

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,19 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HttpsProtocol {
+
+    public function handle($request, Closure $next)
+    {
+        if (config('app.env') === 'production' or config('app.env') === 'staging') {
+            $request->server->set('HTTPS', 'on');
+            if ($request->header('x-forwarded-proto') <> 'https') {
+                return redirect()->secure($request->getRequestUri());
+            }
+        }
+
+        return $next($request);
+    }
+}
