39c4b06419aefd07323fc1363a7a6a842c5b401a
tarokimura87 <taro.palooza.08@gmail.com>
2019-11-20 13:37:56 +0900

    make it better

--- config/import.php
+++ config/import.php
@@ -158,16 +158,16 @@ return [
 //        'import' => HospitalImageImport::class,
 //        'seed' => false,
 //    ],
-    'm_hospital_time.csv' => [
-        'model' => MedicalTreatmentTime::class,
-        'import' => MedicalTreatmentTimeImport::class,
-        'seed' => false,
-    ],
-//    'm_hospital_detail.csv' => [
-//        'model' => HospitalDetail::class,
-//        'import' => HospitalDetailImport::class,
+//    'm_hospital_time.csv' => [
+//        'model' => MedicalTreatmentTime::class,
+//        'import' => MedicalTreatmentTimeImport::class,
 //        'seed' => false,
 //    ],
+    'm_hospital_detail.csv' => [
+        'model' => HospitalDetail::class,
+        'import' => HospitalDetailImport::class,
+        'seed' => false,
+    ],
 //    'm_course_file.csv' => [
 //        'model' => CourseImage::class,
 //        'import' => CourseImageImport::class,
