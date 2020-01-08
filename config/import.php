a762ee0c393aef707faa5f28e580342c80a6a492
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-08 13:59:14 +0900

    make it better

--- config/import.php
+++ config/import.php
@@ -114,26 +114,26 @@ return [
         'import' => HospitalImport::class,
         'seed' => true,
     ],
-//    'm_hospital_category_dai.csv' => [
-//        'model' => HospitalMajorClassification::class,
-//        'import' => HospitalMajorClassificationImport::class,
-//        'seed' => true,
-//    ],
-//    'm_hospital_category_chu.csv' => [
-//        'model' => HospitalMiddleClassification::class,
-//        'import' => HospitalMiddleClassificationImport::class,
-//        'seed' => true,
-//    ],
-//    'm_hospital_category_sho.csv' => [
-//        'model' => HospitalMinorClassification::class,
-//        'import' => HospitalMinorClassificationImport::class,
-//        'seed' => true,
-//    ],
-//    'm_staff.csv' => [
-//        'model' => Staff::class,
-//        'import' => StaffImport::class,
-//        'seed' => false,
-//    ],
+   'm_hospital_category_dai.csv' => [
+       'model' => HospitalMajorClassification::class,
+       'import' => HospitalMajorClassificationImport::class,
+       'seed' => true,
+   ],
+   'm_hospital_category_chu.csv' => [
+       'model' => HospitalMiddleClassification::class,
+       'import' => HospitalMiddleClassificationImport::class,
+       'seed' => true,
+   ],
+   'm_hospital_category_sho.csv' => [
+       'model' => HospitalMinorClassification::class,
+       'import' => HospitalMinorClassificationImport::class,
+       'seed' => true,
+   ],
+   'm_staff.csv' => [
+       'model' => Staff::class,
+       'import' => StaffImport::class,
+       'seed' => false,
+   ],
     'm_course_basic.csv' => [
         'model' => Course::class,
         'import' => CourseImport::class,
@@ -225,9 +225,9 @@ return [
 //        'import' => \App\Imports\ReservationOptionImport::class,
 //        'seed' => true,
 //    ],
-    'm_hospital_meta.csv' => [
-        'model' => HospitalMeta::class,
-        'import' => HospitalMetaImport::class,
-        'seed' => false,
-    ],
+    // 'm_hospital_meta.csv' => [
+    //     'model' => HospitalMeta::class,
+    //     'import' => HospitalMetaImport::class,
+    //     'seed' => false,
+    // ],
 ];
