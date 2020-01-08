0d03474d3770025039b549be94ea08928bc476ac
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-08 14:13:08 +0900

    make it better

--- config/import.php
+++ config/import.php
@@ -69,26 +69,26 @@ use App\TaxClass;
 
 return [
     // ステージングでのインポートの為、一部コメントアウトしています
-   'm_pref.csv' => [
-       'model' => Prefecture::class,
-       'import' => PrefectureImport::class,
-       'seed' => false,
-   ],
-   'm_tax_class.csv' => [
-       'model' => TaxClass::class,
-       'import' => TaxImport::class,
-       'seed' => false,
-   ],
-   'm_district.csv' => [
-       'model' => DistrictCode::class,
-       'import' => DistrictCodeImport::class,
-       'seed' => false,
-   ],
-   'm_item_type.csv' => [
-       'model' => ClassificationType::class,
-       'import' => ClassificationTypeImport::class,
-       'seed' => true,
-   ],
+   // 'm_pref.csv' => [
+   //     'model' => Prefecture::class,
+   //     'import' => PrefectureImport::class,
+   //     'seed' => false,
+   // ],
+   // 'm_tax_class.csv' => [
+   //     'model' => TaxClass::class,
+   //     'import' => TaxImport::class,
+   //     'seed' => false,
+   // ],
+   // 'm_district.csv' => [
+   //     'model' => DistrictCode::class,
+   //     'import' => DistrictCodeImport::class,
+   //     'seed' => false,
+   // ],
+   // 'm_item_type.csv' => [
+   //     'model' => ClassificationType::class,
+   //     'import' => ClassificationTypeImport::class,
+   //     'seed' => true,
+   // ],
 //    'm_item_category_dai.csv' => [
 //        'model' => MajorClassification::class,
 //        'import' => MajorClassificationImport::class,
