846f733e0bf48f8a5610737b19a3e71c64ee27cc
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-14 11:45:51 +0900

    make it better

--- config/import.php
+++ config/import.php
@@ -145,31 +145,31 @@ return [
     //     'seed' => false,
     // ],
     //
-    'm_course_question.csv' => [
-        'model' => CourseQuestion::class,
-        'import' => CourseQuestionImport::class,
-        'seed' => false,
-    ],
-    'm_file_location.csv' => [
-        'model' => ImageOrder::class,
-        'import' => ImageOrderImport::class,
-        'seed' => false,
-    ],
-    'm_hospital_file.csv' => [
-        'model' => HospitalImage::class,
-        'import' => HospitalImageImport::class,
-        'seed' => false,
-    ],
-    'm_hospital_time.csv' => [
-        'model' => MedicalTreatmentTime::class,
-        'import' => MedicalTreatmentTimeImport::class,
-        'seed' => false,
-    ],
-    'm_hospital_detail.csv' => [
-        'model' => HospitalDetail::class,
-        'import' => HospitalDetailImport::class,
-        'seed' => false,
-    ],
+    // 'm_course_question.csv' => [
+    //     'model' => CourseQuestion::class,
+    //     'import' => CourseQuestionImport::class,
+    //     'seed' => false,
+    // ],
+    // 'm_file_location.csv' => [
+    //     'model' => ImageOrder::class,
+    //     'import' => ImageOrderImport::class,
+    //     'seed' => false,
+    // ],
+    // 'm_hospital_file.csv' => [
+    //     'model' => HospitalImage::class,
+    //     'import' => HospitalImageImport::class,
+    //     'seed' => false,
+    // ],
+    // 'm_hospital_time.csv' => [
+    //     'model' => MedicalTreatmentTime::class,
+    //     'import' => MedicalTreatmentTimeImport::class,
+    //     'seed' => false,
+    // ],
+    // 'm_hospital_detail.csv' => [
+    //     'model' => HospitalDetail::class,
+    //     'import' => HospitalDetailImport::class,
+    //     'seed' => false,
+    // ],
     'm_course_file.csv' => [
         'model' => CourseImage::class,
         'import' => CourseImageImport::class,
