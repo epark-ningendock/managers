5ab54a645721317318f62954a80701c038df1bed
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-21 09:01:28 +0900

    make it better

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -538,7 +538,7 @@ class ReservationService
         $entity->campaign_code = $other_info['campaign_cd'] ?? $entity->campaign_code;
         $entity->tel_timezone = $other_info['tel_timezone'] ?? $entity->tel_timezone;
         $entity->insurance_assoc_id = $other_info['insurer_number'] ?? $entity->insurance_assoc_id;
-        $entity->insurance_assoc = $other_info['insurance_assoc'] ?? $entity->insurance_assoc;       
+        $entity->insurance_assoc = $other_info['insurance_assoc'] ?? $entity->insurance_assoc;
         $entity->mail_type = $process === self::REGISTRATION ? '1' : '2';
         $entity->cancelled_appoint_code = $request->input('cancelled_appoint_code') ?? $entity->cancelled_appoint_code;
 
@@ -671,7 +671,7 @@ class ReservationService
     private function getApplyPlan(int $hospitalId) {
 
         $targetDay = Carbon::today();
-        return HospitalPlan::where('id', $hospitalId)
+        return HospitalPlan::where('hospital_id', $hospitalId)
             ->where('from', '<=', $targetDay)
             ->where(function ($query) use ($targetDay) {
                 $query->where('to', '>=', $targetDay)
