a890369377927bdbaf98c84647d0770b2ee3581a
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-25 03:59:33 +0900

    make it better

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -370,7 +370,7 @@ class ReservationService
     {
         // メールで使用する情報の取得
         $entity = $this->find($reservation->id);
-        $hospital_email_setting = HospitalEmailSetting::find($entity->hospital_id);
+        $hospital_email_setting = HospitalEmailSetting::where('hospital_id', $entity->hospital_id)->first();
 
         \Illuminate\Support\Facades\Log::info('予約ID:'. $reservation->id);
         \Illuminate\Support\Facades\Log::info('医療機関ID:'. $reservation->hospital_id);
