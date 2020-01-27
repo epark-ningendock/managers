f2e805784272db52176e99f1e3f9111115b2d27e
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-27 14:05:05 +0900

    make it better

--- app/Http/Controllers/Api/CourseInfoNotificationController.php
+++ app/Http/Controllers/Api/CourseInfoNotificationController.php
@@ -19,7 +19,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 
-use App\Http\Controllers\Controller;
+use Illuminate\Routing\Controller;
 
 
 class CourseInfoNotificationController extends Controller
