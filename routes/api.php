f25df6816f9ae2ca394e53f7c793765f08765ad8
tarokimura87 <taro.palooza.08@gmail.com>
2020-01-28 12:23:12 +0900

    make it better

--- routes/api.php
+++ routes/api.php
@@ -70,7 +70,7 @@ Route::prefix('v1')->group(function () {
 
 // 以下予約API
 // 予約登録/更新API
-    Route::match(['get', 'post'], 'reservation-store/', 'ReservationApiController@store');
+    Route::post('reservation-store', 'ReservationApiController@store')->name('reservation-api.reservation-store');
 // 予約確認API
     Route::get('reservation-conf', 'ReservationApiController@conf');
 // 予約キャンセルAPI
