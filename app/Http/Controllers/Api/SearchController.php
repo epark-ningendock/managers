3cf25f222c3828265e3eaa394f7b7dc2684f8a28
tarokimura87 <taro.palooza.08@gmail.com>
2020-03-03 10:27:04 +0900

    make it better

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -203,11 +203,6 @@ class SearchController extends ApiBaseController
                 for ($i = 1; $i < count($freewords); $i++) {
                     $q->orWhere('course_metas.category_disease_name', 'like', '%' . $freewords[$i] . '%');
                 }
-
-                $q->orWhere('courses.name', 'like', '%' . $freewords[0] . '%');
-                for ($i = 1; $i < count($freewords); $i++) {
-                    $q->orWhere('courses.name', 'like', '%' . $freewords[$i] . '%');
-                }
             });
         };
 
@@ -442,7 +437,6 @@ class SearchController extends ApiBaseController
                     $q->orWhere('hospital_metas.area_station', 'like', '%'.$freeword.'%');
                     $q->orWhere('course_metas.category_exam_name', 'like', '%'.$freeword.'%');
                     $q->orWhere('course_metas.category_disease_name', 'like', '%'.$freeword.'%');
-                    $q->orWhere('name', 'like', '%'.$freeword.'%');
                 });
             }
         }
