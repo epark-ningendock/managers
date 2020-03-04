2fa5f3b06870d591d47d80db8f454aa04829117f
tarokimura87 <taro.palooza.08@gmail.com>
2020-03-04 09:20:09 +0900

    make it better

--- app/Http/Resources/HospitalContentBaseResource.php
+++ app/Http/Resources/HospitalContentBaseResource.php
@@ -117,7 +117,11 @@ class HospitalContentBaseResource extends Resource
         $texts = $categories->map(function ($t) {
             $title = $t->title ?? '';
             $caption = $t->caption ?? '';
-            return collect(['title' => $title, 'caption' => strstr($caption, '<p class="s-movie">', true),]);
+
+            if (strstr($caption, '<p class="s-movie">', true)) {
+                $caption = strstr($caption, '<p class="s-movie">', true);
+            }
+            return collect(['title' => $title, 'caption' => $caption,]);
         });
         return $texts->first();
     }
