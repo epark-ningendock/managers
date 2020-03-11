afbf544e4aa14d42aa52ab666bd7b044d2b1ea5a
tarokimura87 <taro.palooza.08@gmail.com>
2020-03-11 22:21:40 +0900

    make it better

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -384,12 +384,12 @@ class SearchController extends ApiBaseController
         $query->orderBy('hospitals.pv_count', 'desc');
 
         // limit/offset
-//            if (!$count_flg && $request->input("return_flag") != 0) {
-        $offset = intval($request->input('return_from')-1);
-        $limit = intval($request->input('return_to')) - $offset;
-        $query->offset(0);
-        $query->limit(20);
-//            }
+            if (!$count_flg && $request->input("return_flag") != 0) {
+                $offset = intval($request->input('return_from')-1);
+                $limit = intval($request->input('return_to')) - $offset;
+                $query->offset($offset);
+                $query->limit($limit);
+            }
 
         $results = $query->get();
 
