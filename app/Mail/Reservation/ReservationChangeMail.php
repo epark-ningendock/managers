4b28410a8ae5c7fbf3634e87d9773cc4a9a36e4c
tarokimura87 <taro.palooza.08@gmail.com>
2020-04-07 22:00:59 +0900

    make it better

---
+++ app/Mail/Reservation/ReservationChangeMail.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Mail;
+
+class ReservationChangeMail extends ReservationMail
+{
+    /**
+     * 予約 変更メール
+     *
+     * @return void
+     */
+    public function __construct($entity, $customer_flg)
+    {
+        // entity set
+        $this->entity = json_decode(json_encode($entity));
+
+        // subject
+        $this->subject = '予約変更';
+   
+        // template view
+        $this->view = 'reservation.email.reservation_change';
+
+        $this->customer_flg = $customer_flg;
+    }
+
+}
