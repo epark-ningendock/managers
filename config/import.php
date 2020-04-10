d216105a6bfe74c9cabe73c9fed736da608fca75
tarokimura87 <taro.palooza.08@gmail.com>
2020-04-10 21:30:35 +0900

    make it better

--- config/import.php
+++ config/import.php
@@ -139,22 +139,22 @@ return [
     //     'import' => CourseImport::class,
     //     'seed' => false,
     // ],
-    'm_course_detail_5.csv' => [
+    'm_course_detail_9.csv' => [
         'model' => CourseDetail::class,
         'import' => CourseDetailImport::class,
         'seed' => false,
     ],
-    'm_course_detail_6.csv' => [
+    'm_course_detail_10.csv' => [
         'model' => CourseDetail::class,
         'import' => CourseDetailImport::class,
         'seed' => false,
     ],
-    'm_course_detail_7.csv' => [
+    'm_course_detail_11.csv' => [
         'model' => CourseDetail::class,
         'import' => CourseDetailImport::class,
         'seed' => false,
     ],
-    'm_course_detail_8.csv' => [
+    'm_course_detail_12.csv' => [
         'model' => CourseDetail::class,
         'import' => CourseDetailImport::class,
         'seed' => false,
