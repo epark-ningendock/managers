f2e2a59c1f0d1eebe9ba29b5de355b8a9b792062
tarokimura87 <taro.palooza.08@gmail.com>
2020-04-16 01:23:44 +0900

    make it better

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -568,11 +568,11 @@ class ReservationController extends Controller
             }
             // 医療機関へメール送信
             if ($change_flg) {
-                Mail::to($fax_tos)->send(new ReservationChangeFaxToMail($reservation));
+                Mail::to($tos)->send(new ReservationChangeFaxToMail($reservation));
             } elseif ($reservation->reservation_status == ReservationStatus::CANCELLED) {
-                Mail::to($fax_tos)->send(new ReservationCancelFaxToMail($reservation));
+                Mail::to($tos)->send(new ReservationCancelFaxToMail($reservation));
             } elseif ($reservation->reservation_status == ReservationStatus::RECEPTION_COMPLETED) {
-                Mail::to($fax_tos)->send(new ReservationReceptionCompleteFaxToMail($reservation));
+                Mail::to($tos)->send(new ReservationReceptionCompleteFaxToMail($reservation));
             }
         }
     }
