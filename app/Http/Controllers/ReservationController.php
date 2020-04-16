c5f70cbc0ed114bb73f7fff2488fac4d9535ef60
tarokimura87 <taro.palooza.08@gmail.com>
2020-04-16 18:44:16 +0900

    make it better

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -91,7 +91,7 @@ class ReservationController extends Controller
         $page_per_record = $request->input('record_per_page', 10);
 
         $query = $this->get_reception_list_query($request);
-        $reservations = $query->paginate($page_per_record)
+        $reservations = $query->withTrashed()->paginate($page_per_record)
             ->appends($request->query());
         $courses = Course::where('hospital_id', session()->get('hospital_id'))->get();
 
@@ -124,7 +124,7 @@ class ReservationController extends Controller
     protected function get_reception_list_query(Request $request)
     {
         // dd($request->all());
-        $query = Reservation::withTrashed()->where('hospital_id', session('hospital_id'))->with([
+        $query = Reservation::where('hospital_id', session('hospital_id'))->with([
             'course',
             'customer',
             'reservation_options',
