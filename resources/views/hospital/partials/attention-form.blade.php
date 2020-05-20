b64f06eecb40a17bdded3c60e7e31c65ce18ba84
tarokimura87 <taro.palooza.08@gmail.com>
2020-05-20 18:06:52 +0900

    make it better

--- resources/views/hospital/partials/attention-form.blade.php
+++ resources/views/hospital/partials/attention-form.blade.php
@@ -152,10 +152,10 @@ $o_minor_values = collect(old('minor_values'));
 
                   <legend>オプションプラン</legend>
 
-              <div class="form-group margin-none py-sm-2">
+              <div class="form-group margin-none py-sm-1">
                       <input type="hidden" name="dr_movie" value="0"/>
                       <input type="checkbox" id="dr_movie" name="dr_movie" value="1"  @if( $dr_movie == 1) checked @endif />
-                      <label for="dr_movie">Dr.動画　（16,500円）</label>
+                      Dr.動画　（16,500円）
                   　　<input class="form-control w8em" type="number" id="dr_movie_price" name="dr_movie_price" value="{{ old('dr_movie_price', (isset($dr_movie_price_price) ? $dr_movie_price_price : 0)) }}">円
 
               </div>
