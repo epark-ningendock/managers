17dc8cedb24a7409d56d9beee382707da74ea4bb
tarokimura87 <taro.palooza.08@gmail.com>
2020-05-27 13:29:39 +0900

    make it better

--- app/Services/ReservationService.php
+++ app/Services/ReservationService.php
@@ -344,6 +344,8 @@ class ReservationService
         $reception_end_date = Carbon::today();
         $reception_end_date->addMonthsNoOverflow($end_month);
         $reception_end_date->addDays($end_day);
+        \Illuminate\Support\Facades\Log::info('カレンダー確認(受付開始日)：' . $reception_start_date);
+        \Illuminate\Support\Facades\Log::info('カレンダー確認(受付終了日)：' . $reception_end_date);
         if ($reception_start_date > $reservation_date
             || $reception_end_date < $reservation_date
         ) {
