a716308915a1f1ed22675f4d871c70533cbd329c
tarokimura87 <taro.palooza.08@gmail.com>
2020-05-29 17:05:56 +0900

    make it better

--- resources/views/billing/claim-check-pdf.blade.php
+++ resources/views/billing/claim-check-pdf.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="en">
 @php
-use App\Enums\ReservationStatus;
+    use App\Enums\ReservationStatus;
 @endphp
 <head>
 
@@ -272,18 +272,20 @@ use App\Enums\ReservationStatus;
 
     </table>
 
+    <br>
+    @if ($billing->hospital->payment_type == 0)
+        【振込先】 銀行名：りそな銀行　　　　　　　　<br>
+        　　　　　　支店名：市ヶ谷支店　　　　　 　　<br>
+        　　　　　　預金種目：普通預金　　　　 　　　<br>
+        　　　　　　口座番号：1659966　　　　　　 　<br>
+        　　　　　　口座名義：エンパワーヘルスケア(カ<br>
+    @elseif($billing->hospital->payment_type == 1)
+        【引落名義】 エンパワーヘルスケア 株式会社　　<br>
+        　　　　　　 エンパワーヘルスケア（カ　　 　　<br>
+    @endif
+
 </div>
-<br>
-@if ($billing->hospital->payment_type == 0)
-    【振込先】 銀行名：りそな銀行　　　　　　　　<br>
-    　　　　　　支店名：市ヶ谷支店　　　　　　　　<br>
-    　　　　　 預金種目：普通預金　　　　　　　　<br>
-    　　　　　 口座番号：1659966　　　　　　　　<br>
-    　　　　　 口座名義：エンパワーヘルスケア（カ<br>
-@elseif($billing->hospital->payment_type == 1)
-    【引落名義】 エンパワーヘルスケア 株式会社　　<br>
-    　　　　　　　 エンパワーヘルスケア（カ　　　　<br>
-@endif
+
 
 </body>
 </html>
