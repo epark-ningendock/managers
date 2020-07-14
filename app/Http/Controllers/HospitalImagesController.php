09f91ebc5e295f9d37ec78caff88206be9558afa
tarokimura87 <taro.palooza.08@gmail.com>
2020-07-14 13:13:59 +0900

    make it better

--- app/Http/Controllers/HospitalImagesController.php
+++ app/Http/Controllers/HospitalImagesController.php
@@ -165,7 +165,7 @@ class HospitalImagesController extends Controller
         for($i = 1; $i <= 4; $i++){
 //            if(isset($file['sub_'.$i])  or isset($file['sub_'.$i.'_category_id'])) {
                 $this->hospitalImageUploader($file, 'sub_', $i, $hospital, $hospital_id,ImageGroupNumber::IMAGE_GROUP_FACILITY_SUB);
-                Log::error('画像あり：！！！！！！！！！！！！！！！！！');
+                Log::error('画像あり：！！！！！！！！！！！！！！！！！' . $file['sub_'.$i.'_category_id']);
 //            }
         }
         //こだわり
