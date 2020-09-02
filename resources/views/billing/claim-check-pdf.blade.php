0ff8a1da94f9588bab6ae186d3e8b4ef961b0f20
maezawa0704 <maezawa@eparkdock.com>
2020-09-02 11:44:02 +0900

    送信メール履歴機能追加

--- resources/views/billing/claim-check-pdf.blade.php
+++ resources/views/billing/claim-check-pdf.blade.php
@@ -12,6 +12,18 @@
 
     <title>請求確認PDF</title>
     <style>
+        @font-face{
+            font-family: ipag;
+            font-style: normal;
+            font-weight: normal;
+            src:url('{{ storage_path('fonts/ipag.ttf')}}');
+        }
+        @font-face{
+            font-family: ipag;
+            font-style: bold;
+            font-weight: bold;
+            src:url('{{ storage_path('fonts/ipag.ttf')}}');
+        }
         body *{
             font-family: ipag;
 
