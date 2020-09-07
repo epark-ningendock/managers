de919226baca1022c614cc1187c54be5e1251d67
maezawa0704 <maezawa@eparkdock.com>
2020-09-07 16:14:16 +0900

    For Debug json parse error

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -142,6 +142,7 @@ class SearchController extends ApiBaseController
                 + compact('courses'))->setCallback($callback);
         } catch (\Exception $e) {
             Log::error($e);
+						Log::error($courses);
             return $this->createResponse($this->messages['system_error_api'], $request->input('callback'));
         }
 
