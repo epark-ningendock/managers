25693d592746195abe26e2cfaa17279808af196b
maezawa0704 <maezawa@eparkdock.com>
2020-09-08 13:01:09 +0900

    debug for search controller

--- app/Http/Controllers/Api/SearchController.php
+++ app/Http/Controllers/Api/SearchController.php
@@ -134,7 +134,7 @@ class SearchController extends ApiBaseController
             $courses = SearchCoursesResource::collection($entities);
 
             // response
-						Log::info($courses);
+						Log::info($courses->all());
             return $search_condition_return_flag == 0 ?
                 response()->json(compact('status', 'search_count', 'return_count', 'return_from', 'return_to', 'courses'))
                     ->setCallback($callback)
