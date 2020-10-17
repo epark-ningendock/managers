b1b5db77053d68216ff0f877414320d8cebc66f5
maezawa0704 <maezawa@eparkdock.com>
2020-10-17 09:13:47 +0900

    メンテ

--- routes/web.php
+++ routes/web.php
@@ -10,8 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::any('{all}', function(){ abort('503'); })->where('all', '.*');
-
+if($_SERVER["REMOTE_ADDR"] !== '106.72.140.160'){
+	Route::any('{all}', function(){ abort('503'); })->where('all', '.*');
+}
 
 Route::get('/', function () {
     return view('index');
