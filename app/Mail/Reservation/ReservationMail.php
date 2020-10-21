e87bf721c200f886de1ea53578f4f36a941988d1
maezawa0704 <maezawa@eparkdock.com>
2020-10-21 12:21:04 +0900

    fix

--- app/Mail/Reservation/ReservationMail.php
+++ app/Mail/Reservation/ReservationMail.php
@@ -109,7 +109,7 @@ class ReservationMail extends Mailable
 				$_operator = session()->get('isEpark');
 				$operator = '';
 				\Log::info($this->entity->terminal_type);
-				if($this->entity->terminal_type == TerminalType::PC || $this->entity->terminal_type == TerminalType::SMART_PHONE){
+				if($this->entity->terminal_type === 2 || $this->entity->terminal_type === 3){
 					$operator = 'From:C';
 					if($this->entity->site_code != '') $operator .= "（{$this->entity->site_code}）";
 				}elseif($_operator == 'false'){
